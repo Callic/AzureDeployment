f1593a2f51fcc37c726fb043629908cd126f831d
lumen\gustavo <gustavocallic@hotmail.com>
2023-03-17 11:07:27 -0300

    .3

--- AzureDeployment/Controllers/WeatherForecastController.cs
+++ AzureDeployment/Controllers/WeatherForecastController.cs
@@ -18,16 +18,13 @@ namespace AzureDeployment.Controllers
             _logger = logger;
         }
 
+
+        
+
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new RedirectResult("~/swagger");
         }
     }
 }
